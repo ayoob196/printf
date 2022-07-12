987fdeac498066f01d3a67278614bb317b2167e2
moratiwap <moratiwapearl@gmail.com>
2022-07-12 08:56:28 -0700

    rebuilding everything from scrach.

--- _printf.c
+++ _printf.c
@@ -12,9 +12,8 @@ int _printf(const char *format, ...)
 	const char *p;
 	va_list arguments;
 	flags_t flags = {0, 0, 0};
-	
+
 	register int count = 0;
-	
 	va_start(arguments, format);
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
