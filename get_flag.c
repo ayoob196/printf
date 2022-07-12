7e14d656c84674bb8c89894bdd4061665a395733
ayoob <ayoobsaad5@gmail.com>
2022-07-12 05:43:04 -0700

    rebuilding everything from scrach

---
+++ get_flag.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * get_flag - a
+ * @s: a
+ * @f: a
+ * Return: a
+ */
+int get_flag(char s, flags_t *f)
+{
+	int i = 0;
+
+	switch (s)
+	{
+		case '+':
+			f->plus = 1;
+			i = 1;
+			break;
+		case ' ':
+			f->space = 1;
+			i = 1;
+			break;
+		case '#':
+			f->hash = 1;
+			i = 1;
+			break;
+	}
+	return (i);
+}
